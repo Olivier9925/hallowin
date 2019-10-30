1fd4fabe955fa04a2189a54611903c6e557e3381
AurelieMio <aureliemiomandre@hotmail.fr>
2019-10-30 14:00:39 +0100

    nouvelle branche card

---
+++ hallowin/src/components/Card.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import shield from "../graphics/shield.png"
+import blood from "../graphics/blood.png"
+import crown from "../graphics/crowns.png"
+import "./Card.css"
+
+const vignetteStyle = {
+    width : `20px`,
+    height : `20px`,
+    top: `10px`
+}
+const levelStyle = {
+    width : `30px`,
+    height : `30px`,
+    top: `10px`
+}
+class Card extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        const {name,
+            level,
+            attack,
+            defense,
+            picture,
+            special} = this.props;
+        return(
+            <div id="cardBackground" style = {{backgroundImage : `url(${picture})`}}>
+                <div id="cardStats">
+                    <div id="cardLevel" style = {{backgroundImage : `url(${crown})`, ...levelStyle}}>
+                        {level}</div>
+                    <div id="cardAttack" style = {{backgroundImage : `url(${blood})`, ...vignetteStyle}}>
+                        {attack}</div>
+                    <div id="cardDefense" style = {{backgroundImage : `url(${shield})`, ...vignetteStyle}}>
+                        {defense}</div>
+                </div>
+                <div id="cardDescription">
+                    <p>{name}</p>
+                    <p>{special}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
