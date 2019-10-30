3554eade9c10af1d7d591d4590e0cfa80f299231
Emilie Pujol <emilie.pujol@protonmail.com>
2019-10-30 15:39:03 +0100

    ajout modales lose/win

--- hallowin/src/components/Game.js
+++ hallowin/src/components/Game.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import AfficherDeck from "./AfficherDeck";
+import Lose from './Lose';
+import Win from './Win'
 
 class Game extends React.Component {
     constructor() {
@@ -12,6 +14,8 @@ class Game extends React.Component {
             computerCurrentCard: null,
             playerLifePoints: 15,
             computerLifePoints: 15,
+            win: false,
+            lose: false,
         }
         this.getMonster = this.getMonster.bind(this)
         this.battle = this.battle.bind(this)
@@ -40,20 +44,26 @@ class Game extends React.Component {
                 this.setState({ 
                     computerLifePoints: this.state.computerLifePoints - (playerCurrentCard.attack - computerCurrentCard.defense)
                 })
+                if (this.state.computerLifePoints <= 0) {
+                    this.setState({ win: true })
+                }
             }
             if (computerCurrentCard.attack > playerCurrentCard.defense) {
                 this.setState({
                     playerLifePoints: this.state.playerLifePoints - (computerCurrentCard.attack - computerCurrentCard.defense)
                 })
-            }
-            
+                if (this.state.playerLifePoints <= 0) {
+                    this.setState({ lose: true })
+                }
+            }           
     }
 
     render() {
         return (
         <div>
             <AfficherDeck playerStack={this.state.playerStack} />
-
+            {this.state.lose && <Lose />}
+            {this.state.win && <Win />}
         </div>
         )
     }
