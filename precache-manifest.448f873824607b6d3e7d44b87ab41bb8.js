a6108a59213f73cd25ce2cef2b4280aedd0f553b
AurelieMio <aureliemiomandre@hotmail.fr>
2019-10-31 13:18:06 +0100

    Updates

---
+++ precache-manifest.448f873824607b6d3e7d44b87ab41bb8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4667655f4fed86a1ab97a59d623bafc6",
+    "url": "/hallowin/index.html"
+  },
+  {
+    "revision": "bc2e53e25c8d24aebd09",
+    "url": "/hallowin/static/css/main.7857bc72.chunk.css"
+  },
+  {
+    "revision": "0715a2c988a3f575940b",
+    "url": "/hallowin/static/js/2.d39cdd55.chunk.js"
+  },
+  {
+    "revision": "bc2e53e25c8d24aebd09",
+    "url": "/hallowin/static/js/main.39ac07b6.chunk.js"
+  },
+  {
+    "revision": "8d6680f19ff0f169fdf3",
+    "url": "/hallowin/static/js/runtime-main.47fbf890.js"
+  },
+  {
+    "revision": "cd17e2c6b2c9d0b9e629f2bde6aba8b6",
+    "url": "/hallowin/static/media/Hero.cd17e2c6.png"
+  },
+  {
+    "revision": "6ac355ec528ab4749a33009fff159c62",
+    "url": "/hallowin/static/media/Madjoe.6ac355ec.ttf"
+  },
+  {
+    "revision": "56ed731d99d30f0d545943ae7f250d72",
+    "url": "/hallowin/static/media/Potion_2.56ed731d.png"
+  },
+  {
+    "revision": "7724229141cce878244a6bd086435a0d",
+    "url": "/hallowin/static/media/SwordAxe.77242291.png"
+  },
+  {
+    "revision": "3613c2dac96217d099ff50d3b8c1472b",
+    "url": "/hallowin/static/media/blood.3613c2da.png"
+  },
+  {
+    "revision": "553eb408359c637366253885d611abf3",
+    "url": "/hallowin/static/media/candle.553eb408.png"
+  },
+  {
+    "revision": "22321647fae1e6023eab338a5d5a6de0",
+    "url": "/hallowin/static/media/card_back.22321647.jpg"
+  },
+  {
+    "revision": "6179c1046c5b93880adcc9c081e0d932",
+    "url": "/hallowin/static/media/rock3.6179c104.jpg"
+  },
+  {
+    "revision": "e15e53adc26e6fdfcb8b2063207d61e9",
+    "url": "/hallowin/static/media/shield_start.e15e53ad.png"
+  },
+  {
+    "revision": "9dd50d8b404e66ba3890e8ef5cf92041",
+    "url": "/hallowin/static/media/spiderWeb.9dd50d8b.png"
+  },
+  {
+    "revision": "42d70ff748518c7743b278f6dc1e40c5",
+    "url": "/hallowin/static/media/wood1.42d70ff7.png"
+  },
+  {
+    "revision": "5d6cc8808055122dbdeb297e07058110",
+    "url": "/hallowin/static/media/wood2.5d6cc880.png"
+  },
+  {
+    "revision": "6d0cd2aa51797d9fed3365451a457447",
+    "url": "/hallowin/static/media/youdied.6d0cd2aa.jpg"
+  },
+  {
+    "revision": "85fe93a92b2bb38fe2091e331a846434",
+    "url": "/hallowin/static/media/youwin.85fe93a9.png"
+  }
+]);
